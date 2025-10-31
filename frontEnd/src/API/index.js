dde315315852302033828661c9d857c76bb3d645
jason <jasonraym8@gmail.com>
2025-10-31 14:49:24 +0800

    Added API calls

--- frontEnd/src/API/index.js
+++ frontEnd/src/API/index.js
@@ -1,48 +1,66 @@
 import axios from 'axios'
 
-export const handleLogin = () => {
-    axios.post('http://localhost:5000/login')
-    .then(res => {console.log(res)})
+export const handleLogin = (values, nav) => {
+    axios.post('http://localhost:5000/login', values)
+    .then(res => {console.log(res); localStorage.setItem('token', res.data.token); nav('/home')})
     .catch(err => {console.log(err)})
 }
 
-export const handleRegister = () => {
-    axios.post('http://localhost:5000/register')
-    .then(res => {console.log(res)})
+export const handleRegister = (values, nav) => {
+    axios.post('http://localhost:5000/register', values)
+    .then(res => {console.log(res); nav('/')})
     .catch(err => {console.log(err)})
 }
 
 export const handleReadAll = (setVarName) => {
-    axios.get('http://localhost:5000/view/cards')
+    axios.get('http://localhost:5000/view/cards', {headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}})
     .then(res => {console.log(res); setVarName(res.data)})
     .catch(err => {console.log(err)})
 }
 
 export const handleReadSingleCard = (id, setVarName) => {
-    axios.get('http://localhost:5000/view/'+id)
-    .then(res => {console.log(res); setVarName({rfidType: res.data[0].rfidType, 
-                                                nickName: res.data[0].nickName, 
-                                                cardNumber: res.data[0].cardNumber, 
-                                                cardBalance: res.data[0].cardBalance})})
+    axios.get('http://localhost:5000/view/'+id, {headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}})
+    .then(res => {console.log(res); setVarName({rfidType: res.data.rfidType, 
+                                                nickName: res.data.nickName, 
+                                                cardNumber: res.data.cardNumber, 
+                                                cardBalance: res.data.cardBalance})})
+    .catch(err => {console.log(err)})
+}
+
+export const handleReadTotalBalance = (setVarName) => {
+    axios.get('http://localhost:5000/home', {headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}})
+    .then(res => {console.log(res); setVarName(res.data)})
     .catch(err => {console.log(err)})
 }
 
 export const handleEnrollCard = (e, values, nav) => {
     e.preventDefault();
-    axios.post('http://localhost:5000/enrollCard', values)
+    axios.post('http://localhost:5000/enrollCard', values, {headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}})
     .then(res => {console.log(res); nav('/home')})
     .catch(err => {console.log(err)})
 }
 
 export const handleUpdateCard = (e, id, values, nav) => {
     e.preventDefault();
-    axios.put('http://localhost:5000/updateCard/'+id, values)
+    axios.put('http://localhost:5000/updateCard/'+id, values, {headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}})
     .then(res => {console.log(res); nav('/home')})
     .catch(err => {console.log(err)})
 }
 
 export const handleDeleteCard = (id, nav) => {
-    axios.delete('http://localhost:5000/deleteCard/'+id)
+    axios.delete('http://localhost:5000/deleteCard/'+id, {headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}})
     .then(res => {console.log(res); nav('/home')})
+    .catch(err => {console.log(err)})
+}
+
+export const handleTransfer = (id, values, nav) => {
+    axios.post('http://localhost:5000/transfer/source/'+id, values, {headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}})
+    .then(res => {console.log(res); nav('/home')})
+    .catch(err => {console.log(err)})
+}
+
+export const handleTransactionLogs = (setVarName) => {
+    axios.get('http://localhost:5000/transactions', {headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}})
+    .then(res => {console.log(res); setVarName(res.data)})
     .catch(err => {console.log(err)})
 }
