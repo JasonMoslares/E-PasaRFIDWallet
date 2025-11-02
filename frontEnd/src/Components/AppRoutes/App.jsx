703a99d4855c19d334e2cf221a344c26db9404a7
jason <jasonraym8@gmail.com>
2025-11-02 11:54:00 +0800

    Added Header in App

--- frontEnd/src/Components/AppRoutes/App.jsx
+++ frontEnd/src/Components/AppRoutes/App.jsx
@@ -1,4 +1,4 @@
-import {Route, Routes} from 'react-router-dom'
+import {Route, Router, Routes} from 'react-router-dom'
 import Login from '../../Pages/Login';
 import Register from '../../Pages/Register';
 import Home from '../../Pages/Home';
@@ -8,21 +8,25 @@ import ListofCards from '../../Pages/ListofCards';
 import ViewCard from '../../Pages/ViewCard';
 import UpdateCard from '../../Pages/UpdateCard';
 import TransactionLogs from '../../Pages/TransactionLog';
+import Header from '../Header/Header';
 
 
 function AppRoutes(){
     return(
-        <Routes>
-            <Route path="/" element={<Login />}></Route>
-            <Route path="/register" element={<Register />}></Route>
-            <Route path="/home" element={<Home />}></Route>
-            <Route path="/enrollCard" element={<EnrollCard />}></Route>
-            <Route path="/transfer" element={<Transfer />}></Route>
-            <Route path="/view/cards" element={<ListofCards />}></Route>
-            <Route path="/view/:cardNumber" element={<ViewCard />}></Route>
-            <Route path="/updateCard/:cardNumber" element={<UpdateCard />}></Route>
-            <Route path="/transactions" element={<TransactionLogs />}></Route>
-        </Routes>
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Login />}></Route>
+                <Route path="/register" element={<Register />}></Route>
+                <Route path="/home" element={<Home />}></Route>
+                <Route path="/enrollCard" element={<EnrollCard />}></Route>
+                <Route path="/transfer" element={<Transfer />}></Route>
+                <Route path="/view/cards" element={<ListofCards />}></Route>
+                <Route path="/view/:cardNumber" element={<ViewCard />}></Route>
+                <Route path="/updateCard/:cardNumber" element={<UpdateCard />}></Route>
+                <Route path="/transactions" element={<TransactionLogs />}></Route>
+            </Routes>
+        </Router>
     );
 }
 
