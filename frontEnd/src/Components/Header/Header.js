d3fee6d2909b66d6f74b93b018ff8bf5082c415f
jason <jasonraym8@gmail.com>
2025-11-02 11:49:54 +0800

    Added Header Component

---
+++ frontEnd/src/Components/Header/Header.js
@@ -0,0 +1,43 @@
+import { Link, useNavigate } from "react-router-dom";
+import { handleLogout } from "../../API";
+
+const Header = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    const navigate = useNavigate();
+
+    const handleLogOut = () => {
+        handleLogout(navigate);
+    }
+
+    let headerRightContent;
+    
+    if(user){
+        headerRightContent = (
+            <>
+                <span>Hi, {user.name}</span>
+                <button type='button' className='logOutButton' onCLick={handleLogOut}>Log Out</button>
+            </>
+        )
+    }
+    else{
+        headerRightContent = (
+            <>
+                <Link to='/register'>Register</Link>
+                <Link to='/'>Log In</Link>
+            </>
+        )
+    }
+
+    return(
+        <header>
+            <div className='header-title'>
+                <h2>E-Pasa</h2>
+            </div>
+            <div className="header-right-content">
+                {headerRightContent}
+            </div>
+        </header>
+    );
+}
+
+export default Header
