368410a4b660db515e3aaf9e2ae33c4d3ad437a4
jason <jasonraym8@gmail.com>
2025-11-05 06:37:26 +0800

    Made tiles clickable

--- frontEnd/src/Pages/Home.jsx
+++ frontEnd/src/Pages/Home.jsx
@@ -1,7 +1,8 @@
 
 import {useState, useEffect} from 'react'
-import {Link, useNavigate} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import {Card} from 'antd'
+import {SquarePlus, MousePointer2, IdCard, ClipboardList} from "lucide-react";
 import { handleReadTotalBalance } from "../API"
 
 const Home = () => {
@@ -18,36 +19,46 @@ const Home = () => {
 
     }, [])
 
-    const navigate = useNavigate();
-
     return(
         <>
             <Card>
                 <div className="balance-space">
-                    <Card>
+                    <Card className="balance-card">
                         <div className="balance-title">
                             <h1>Total Balance Across Cards: </h1>
                         </div>
                         <div className="balance-section">
-                            <h2>{balance}</h2>
+                            <h2>₱ {balance}</h2>
                         </div>
                     </Card>
                 </div>
             </Card>
             <Card>
                 <div className="functionalities-space">
-                    <Card>
-                        <Link to='/enrollCard'>Enroll Card</Link>
-                    </Card>
-                    <Card>
-                        <Link to='/transfer/'>Transfer Funds</Link>
-                    </Card>
-                    <Card>
-                        <Link to ='/view/cards'>Cards</Link>
-                    </Card>
-                    <Card>
-                        <Link to='/transactions'>Transaction Logs</Link>
-                    </Card>
+                    <Link to='/enrollCard' className="function-card">
+                        <div className="icon-tile">
+                            <SquarePlus className="function-icon" />
+                            <span className="function-label">Enroll Card</span>
+                        </div>
+                    </Link>
+                    <Link to='/transfer/' className="function-card">
+                        <div className="icon-tile">
+                            <MousePointer2 className="function-icon" />
+                            <span className="function-label">Transfer Funds</span>
+                        </div>
+                    </Link>
+                    <Link to='/view/cards' className="function-card">
+                        <div className="icon-tile">
+                            <IdCard className="function-icon" />
+                            <span className="function-label">Cards</span>
+                        </div>
+                    </Link>
+                    <Link to='/transactions' className="function-card">
+                        <div className="icon-tile">
+                            <ClipboardList className="function-icon" />
+                            <span className="function-label">Transaction Logs</span>
+                        </div>
+                    </Link>
                 </div>
             </Card>
         </>
