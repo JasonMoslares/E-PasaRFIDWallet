8568fae1069785a8b218f02008b829cb848987c4
jason <jasonraym8@gmail.com>
2025-11-06 06:16:05 +0800

    Changed axios to api

--- frontEnd/src/API/index.js
+++ frontEnd/src/API/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
+import api from "./api";
 
 export const handleLogin = (values, nav) => {
-    axios.post('http://localhost:4000/login', values)
+    api.post('http://localhost:4000/login', values)
     .then(res => {console.log(res); 
                 localStorage.setItem('accessToken', res.data.accessToken);
                 localStorage.setItem('refreshToken', res.data.refreshToken);
@@ -11,22 +12,13 @@ export const handleLogin = (values, nav) => {
 }
 
 export const handleRegister = (values, nav) => {
-    axios.post('http://localhost:4000/register', values)
+    api.post('http://localhost:4000/register', values)
     .then(res => {console.log(res); nav('/')})
     .catch(err => {console.log(err)})
 }
 
-export const refreshAccessToken = async () => {
-    axios.post('http://localhost:4000/token', {}, {headers: {'x-refresh-token': localStorage.getItem('refreshToken')}})
-    .then(res => {console.log(res);
-                localStorage.setItem('accessToken', res.data.accessToken);
-                return res.data.accessToken;
-    })
-    .catch(err => {console.log(err)})
-}
-
 export const handleLogout = (nav) => {
-    axios.delete('http://localhost:4000/logout', {headers: {'x-refresh-token': localStorage.getItem('refreshToken')}})
+    api.delete('http://localhost:4000/logout', {headers: {'x-refresh-token': localStorage.getItem('refreshToken')}})
     .then(res => {console.log(res);
                 localStorage.removeItem('accessToken')
                 localStorage.removeItem('refreshToken')
@@ -36,13 +28,13 @@ export const handleLogout = (nav) => {
 }
 
 export const handleReadAll = (setVarName) => {
-    axios.get('http://localhost:5000/view/cards', {headers: {Authorization: `Bearer ${localStorage.getItem('accessToken')}`}})
+    api.get('http://localhost:5000/view/cards')
     .then(res => {console.log(res); setVarName(res.data)})
     .catch(err => {console.log(err)})
 }
 
 export const handleReadSingleCard = (id, setVarName) => {
-    axios.get('http://localhost:5000/view/'+id, {headers: {Authorization: `Bearer ${localStorage.getItem('accessToken')}`}})
+    api.get('http://localhost:5000/view/'+id)
     .then(res => {console.log(res); setVarName({rfidType: res.data.rfidType, 
                                                 nickName: res.data.nickName, 
                                                 cardNumber: res.data.cardNumber, 
@@ -51,43 +43,43 @@ export const handleReadSingleCard = (id, setVarName) => {
 }
 
 export const handleReadTotalBalance = (setVarName) => {
-    axios.get('http://localhost:5000/home', {headers: {Authorization: `Bearer ${localStorage.getItem('accessToken')}`}})
+    api.get('http://localhost:5000/home')
     .then(res => {console.log(res); setVarName(res.data)})
     .catch(err => {console.log(err)})
 }
 
 export const handleEnrollCard = (values, nav) => {
-    axios.post('http://localhost:5000/enrollCard', values, {headers: {Authorization: `Bearer ${localStorage.getItem('accessToken')}`}})
+    api.post('http://localhost:5000/enrollCard', values)
     .then(res => {console.log(res); nav('/home')})
     .catch(err => {console.log(err)})
 }
 
 export const handleUpdateCard = (id, values, nav) => {
-    axios.put('http://localhost:5000/updateCard/'+id, values, {headers: {Authorization: `Bearer ${localStorage.getItem('accessToken')}`}})
+    api.put('http://localhost:5000/updateCard/'+id, values)
     .then(res => {console.log(res); nav('/home')})
     .catch(err => {console.log(err)})
 }
 
 export const handleDeleteCard = (id, nav) => {
-    axios.delete('http://localhost:5000/deleteCard/'+id, {headers: {Authorization: `Bearer ${localStorage.getItem('accessToken')}`}})
+    api.delete('http://localhost:5000/deleteCard/'+id)
     .then(res => {console.log(res); nav('/home')})
     .catch(err => {console.log(err)})
 }
 
 export const handleTransfer = (values, nav) => {
-    axios.post('http://localhost:5000/transfer', values, {headers: {Authorization: `Bearer ${localStorage.getItem('accessToken')}`}})
+    api.post('http://localhost:5000/transfer', values)
     .then(res => {console.log(res); nav('/home')})
     .catch(err => {console.log(err)})
 }
 
 export const handleTransactionLogs = (setVarName) => {
-    axios.get('http://localhost:5000/transactions', {headers: {Authorization: `Bearer ${localStorage.getItem('accessToken')}`}})
+    api.get('http://localhost:5000/transactions')
     .then(res => {console.log(res); setVarName(res.data)})
     .catch(err => {console.log(err)})
 }
 
 export const handleCardTransactionLogs = (id, setVarName) => {
-    axios.get('http://localhost:5000/view/'+id, {headers: {Authorization: `Bearer ${localStorage.getItem('accessToken')}`}})
+    api.get('http://localhost:5000/view/'+id)
     .then(res => {console.log(res); setVarName(res.data)})
     .catch(err => {console.log(err)})
 }
